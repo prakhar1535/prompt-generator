20488a5dbea80dc18a02c9bcaab73d5d006d4c69
Prakhar Sharma <113304996+prakhar1535@users.noreply.github.com>
2023-10-07 02:18:53 +0530

    build error fixed

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -2,7 +2,7 @@
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 import React, { useState } from 'react'
-import Form from '@components/form'
+import Form from '@components/Form'
 
 
 const CreatePrompt = () => {
